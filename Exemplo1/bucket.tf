5770627edea64b37648f9568621301cc8a3f4e3e
Edwanderson Pereira <80295393+Edwanderson94@users.noreply.github.com>
2023-01-10 14:25:58 -0300

    Add files via upload

---
+++ Exemplo1/bucket.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "howcanisee" {
+  bucket = "bp-bprspbuck1"
+}
+
+# resource "aws_s3_bucket" "first_bucket" {
+#  bucket = "exemplo-curso-terraform-cli"
+# }
---
+++ aws-bucket/bucket.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "edevops-95175362486" {
+  bucket = "the-walking-dead-1994"
+}
