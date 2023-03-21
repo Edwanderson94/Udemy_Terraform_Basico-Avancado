a0dd58fb872700ff97b37b09f1306000e471391c
unknown <edi171194@gmail.com>
2023-03-21 09:45:52 -0300

    atualização aula 34 e 35

--- azure-vm-com-vnet-remote-state/vm.tf
+++ azure-vm-com-vnet-remote-state/vm.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "resource_group" {
   name     = "vm"
   location = var.location
-  tags = local.common_tags
+  tags     = local.common_tags
 }
 
 resource "azurerm_public_ip" "public_ip" {
@@ -22,22 +22,22 @@ resource "azurerm_network_interface" "network_interface" {
     name                          = "public-ip-terraform"
     subnet_id                     = data.terraform_remote_state.vnet.outputs.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-   tags = local.common_tags
+  tags = local.common_tags
 }
 
 resource "azurerm_network_interface_security_group_association" "nisga" {
   network_interface_id      = azurerm_network_interface.network_interface.id
-  network_security_group_id = data.terraform_remote_state.vnet.outputs.network_security_group_id
+  network_security_group_id = data.terraform_remote_state.vnet.outputs.security_group_id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-terraform"
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = "terraform"
+  name                  = "vm-terraform"
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  location              = var.location
+  size                  = "Standard_B1s"
+  admin_username        = "terraform"
   network_interface_ids = [azurerm_network_interface.network_interface.id]
 
   admin_ssh_key {
@@ -56,5 +56,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-   tags = local.common_tags
+  tags = local.common_tags
 }
