9f635c1748164beeb8875834cc57cda151cd6a0d
unknown <edi171194@gmail.com>
2023-06-07 08:50:39 -0300

    ajuste seção 3 do curso

--- Exemplo1/bucket.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "howcanisee" {
-  bucket = "bp-bprspbuck1"
-}
-
-# resource "aws_s3_bucket" "first_bucket" {
-#  bucket = "exemplo-curso-terraform-cli"
-# }
--- extras-terraform/replace-resources/README.md
+++
@@ -1,2 +0,0 @@
-foi utilizado a apsta azure-vnet para executar esse comando.
-documentação: https://developer.hashicorp.com/terraform/cli/commands/taint
