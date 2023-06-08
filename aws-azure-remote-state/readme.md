76131d75b69885e8334ebc2990bb84e543cb7884
unknown <edi171194@gmail.com>
2023-06-08 17:55:28 -0300

    ajuste dos arquivos da seção 7

--- aws-azure-remote-state/readme.md
+++
@@ -1,4 +0,0 @@
-Módulo 5 - Terraform State
-Criação de um storage account na azure com versionamento ativado.
-Criação de um bucket s3 na aws com versionamento ativado.
-Ambos farão armazenamento de remote states. 
--- condicional-expression-aws-vpc/network.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "vpc" {
-    count = var.enviroment == "prd" ? 1 : 0 # Dá para se usar na pipeline a automatização com este recurso
-    
-    cidr_block = "10.0.0.0/16"
-
-    tags = {
-        "name" = "vpc-terraform"
-    } 
-}
